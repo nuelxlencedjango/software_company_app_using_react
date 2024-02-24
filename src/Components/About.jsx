8e68e87d2872b4399fdacfd45047e0eae3eb8692
nuelxlencedjango <nuelxlence@gmail.com>
2024-02-24 16:36:43 +0100

    about us

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,80 +1,76 @@
 import React from "react";
-import abt from '../assets/gwyat.jpeg';
+import abt from '../assets/informationT3.jpeg';
+import why_us from '../assets/informationT2.jpeg';
 
-const About =() =>{
-
-    return(
-        <div>
-            {/* about us text */}
-            <div className="px-4 lg:px-14 py-20 max-w-screen-2x1 mx-auto my-8" id="about">
-                <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
-                    <div>
-                        <img src={abt} alt="" />
-                    </div>
-                    <div className="md:w-3/5 mx-auto">
-                        <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">
-                            The unseen of spending three years at raddotech
-                        </h2>
-                        <p className="md:w-3/4 text-sm text-neutralGrey mb-8">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, optio. Totam aspernatur, veritatis cum nihil ipsum pariatur sunt rerum maxime enim officia suscipit corporis ad repudiandae eius quod ut nisi. Officiis, aliquam provident suscipit non laboriosam molestiae qui ea? Ad nesciunt amet ipsam dignissimos numquam enim officia consequuntur aperiam aliquid?
-                        </p>
-                        <button className="btn-primary">Learn Mire</button>
-                    </div>
-                </div>
-
-            </div>
-            {/* company stats */}
-            <div className="px-4 lg:px-14 max-w-screen-2x1 mx-auto bg-neutralSilver py-16">
-                <div className="flex flex-col md:flex-row justify-between items-center gap-8">
-                    <div className="md:w-1/2">
-                    <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">
-                        Helping a local <br /> <span className="text-brandPrimary">business reinvent itself</span>
-                        </h2>
-                    <p className="md:w-3/4 text-sm text-neutralGrey mb-8">
-                        We reached here with our hard work and dedication
-                           </p>
-                    </div>
-
-                    {/* stats */}
-                    <div className="md:w-1/2 mx-auto flex sm:flex-row flex-col sm:items-center justify-around gap-12">
-                        <div className="space-y-8"> 
-                            <div className="flex items-center gap-4">
-                                <img src="/src/assets/icons/member.png" alt="" />
-                                <div>
-                                    <h4 className="text-2x1 text-neutralDGrey font-semibold">245,845</h4>
-                                    <p>Members</p>
-                                </div>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <img src="/src/assets/icons/member.png" alt="" />
-                                <div>
-                                    <h4 className="text-2x1 text-neutralDGrey font-semibold">2,349</h4>
-                                    <p>Members</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="space-y-8"> 
-                            <div className="flex items-center gap-4">
-                                <img src="/src/assets/icons/member.png" alt="" />
-                                <div>
-                                    <h4 className="text-2x1 text-neutralDGrey font-semibold">3410</h4>
-                                    <p>Members</p>
-                                </div>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <img src="/src/assets/icons/member.png" alt="" />
-                                <div>
-                                    <h4 className="text-2x1 text-neutralDGrey font-semibold">6783</h4>
-                                    <p>Members</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+const About =() => {
+  return (
+    <div>
+      {/* About us text */}
+      <div className="px-4 lg:px-14 py-10 md:py-20 max-w-screen-2x1 mx-auto my-8 bg-neutralSilver" id="about">
+        <div className="flex justify-center items-center h-full">
+          <h2 className="text-4xl text-neutralDGrey font-semibold md:w-4/5 py-5 md:py-14">Welcome to Raddotech</h2>
         </div>
-    )
+        <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
+          <div className="md:w-1/2 mx-auto">
+            <div className="h-64 md:h-auto overflow-hidden">
+              <img src={abt} alt="" className="w-full h-full object-cover" />
+            </div>
+          </div>
+          <div className="md:w-1/2 mx-auto">
+            <p className="md:w-4/5 text-neutralGrey mb-8">
+              At Raddotech, we are passionate about leveraging technology to transform ideas into innovative solutions.
+              With years of experience in software development, our team specializes in creating cutting-edge
+              applications, robust web services, and efficient data solutions tailored to meet the unique needs of
+              our clients.
+            </p>
+            <h2 className="text-2xl text-neutralDGrey font-semibold">Our Mission</h2>
+            <p className="md:w-4/5 text-neutralGrey mb-8 py-5">
+              Our mission is to empower businesses and individuals by providing them with top-notch software solutions
+              that drive growth, enhance efficiency, and streamline operations. We strive to exceed expectations and
+              deliver excellence in every project we undertake.
+            </p>
+            <button className="btn-primary">Learn More</button>
+          </div>
+        </div>
+      </div>
+
+      {/* Why Choose Raddotech */}
+      <div className="px-4 lg:px-20 py-0 max-w-screen-2x1 mx-auto my-0">
+        <div className="flex justify-center items-center h-full">
+          <h2 className="text-4xl text-neutralDGrey font-semibold md:w-4/5 py-0">Why Choose Raddotech?</h2>
+        </div>
+        <p className="md:w-5/6 text-neutralGrey mb-8 py-5 mb-3">
+          Whether you're a startup looking to build your first app, a business in need of a comprehensive web solution,
+          or an organization seeking to harness the power of data, Raddotech is here to partner with you on your journey
+          towards success.
+        </p>
+      </div>
+
+      {/* Innovative Approach */}
+      <div className="px-4 lg:px-14 py-0 max-w-screen-2x1 mx-auto my-8 flex flex-col md:flex-row justify-between items-center gap-12 bg-neutralSilver mb-25" style={{ marginBottom: '100px' }}>
+        <div className="md:w-1/2 mx-auto order-2 md:order-1">
+          <h4 className="text-2xl text-neutralDGrey font-semibold">Innovative Approach</h4>
+          <p className="md:w-4/5 text-neutralGrey py-2 mb-">
+            We embrace innovation and stay ahead of the curve by adopting the latest technologies and best
+            practices in software development.
+          </p>
+          <h4 className="text-2xl text-neutralDGrey font-semibold">Client-Centric Focus</h4>
+          <p className="md:w-4/5 text-neutralGrey py-2 mb-3">
+            Our dedicated team collaborates closely with clients to understand their objectives, challenges, and
+            vision, ensuring that we deliver solutions that align with their goals and exceed their expectations.
+          </p>
+          <h4 className="text-2xl text-neutralDGrey font-semibold">Quality Assurance</h4>
+          <p className="md:w-4/5 text-neutralGrey py-2 mb-3">
+            Quality is at the forefront of everything we do. We adhere to stringent quality standards and rigorous
+            testing processes to ensure that our solutions are reliable, secure, and perform optimally.
+          </p>
+        </div>
+        <div className="md:w-1/2 mx-auto order-1 md:order-2">
+          <img src={why_us} alt="" className="w-full h-auto md:h-full object-cover" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
