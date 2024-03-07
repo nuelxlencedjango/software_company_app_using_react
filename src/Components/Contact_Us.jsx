703f41bddb87176f39df2e049f7eccc6d281879e
nuelxlencedjango <nuelxlence@gmail.com>
2024-03-07 05:00:09 +0100

    animations

--- src/Components/Contact_Us.jsx
+++ src/Components/Contact_Us.jsx
@@ -4,25 +4,28 @@ const Contact_Us = () => {
   return (
     <div>
       <h1 className="text-3xl lg:text-3xl text-[#263238] reach-out font-medium text-center mb-2" data-aos="fade-up">Reach Out To Us Today!</h1>
+      <p className="text-text-[#263238] text-center">ContactUs@raddotech.com</p>
       <div className="mx-4 flex py-10 flex-col md:flex-row justify-center items-start">
         {/* Address Section */}
         <div className="bg-[#cf2e2e] text-white py-12 w-full md:w-1/3 mb-8 mx-2 hover:bg-blue-300 cursor-pointer 
         h-72 flex justify-center items-center" data-aos="fade-right">
           <div className="text-center">
             <h2 className="text-3xl font-semibold mb-4">Address</h2>
-            <p className="text-white">123 Main Street</p>
-            <p className="text-white">City, State ZIP</p>
-            <p className="text-white">Country</p>
+            <p className="text-white">1920 W Villa Maria Road</p>
+            <p className="text-white">Bryan, Texas</p>
+            <p className="text-white">TX 77807</p>
+            <p className="text-white">USA</p>
           </div>
         </div>
+
 
         {/* Phone Numbers Section */}
         <div className="bg-[#000] text-white py-12 w-full md:w-1/3 mb-8 mx-2 hover:bg-green-300 
         cursor-pointer h-72 flex justify-center items-center" data-aos="fade-left">
           <div className="text-center">
             <h2 className="text-3xl font-semibold mb-4">Phone Numbers</h2>
-            <p className="text-white">Main Office: 123-456-7890</p>
-            <p className="text-white">Support: 987-654-3210</p>
+            <p className="text-white">Main Office: +1 (214) 862-2086</p>
+            <p className="text-white">Support: 987-654-32109999999999</p>
           </div>
         </div>
 
@@ -34,16 +37,12 @@ const Contact_Us = () => {
               Contact Us
             </h2>
             <p className="text-white">We'll get back to you within 24hrs</p>
+          
           </div>
             <form className="w-full max-w-md mx-auto mt-4">
               <div className="flex flex-col">
                 <div className="mb-4">
-                  {/* <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="name"
-                  >
-                    Your Name
-                  </label> */}
+                
                   <input
                     className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="name"
@@ -52,12 +51,7 @@ const Contact_Us = () => {
                   />
                 </div>
                 <div className="mb-4">
-                  {/* <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="email"
-                  >
-                    Your Email
-                  </label> */}
+                
                   <input
                     className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="email"
@@ -66,12 +60,7 @@ const Contact_Us = () => {
                   />
                 </div>
                 <div className="mb-4">
-                  {/* <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="message"
-                  >
-                    Message
-                  </label> */}
+                 
                   <textarea
                     className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-20 resize-none"
                     id="message"
