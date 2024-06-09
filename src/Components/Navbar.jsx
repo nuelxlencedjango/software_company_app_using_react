84f693115d7451aa60a9bd0e54af97a77e255089
nuelxlencedjango <nuelxlence@gmail.com>
2024-06-09 06:42:27 +0100

    version changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -47,8 +47,8 @@ const Navbar = () => {
     ]
 
     return(
-       <header className="max-w-full bg-white md:bg-transparent fixed top-0 left-0 right-0" data-aos="fade-right">
-        <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300" :""}`}>
+       <header className="max-w-full bg-white md:bg-transparent fixed top-0 left-0 right-0" data-aos="fade-right"> 
+        <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300" :""}`}> 
             <div className="flex justify-between items-center text-base gap-8">
                 <a href="" className="text-2xl font-semibold flex items-center space-x-3">
                     <img src={logo} alt="" className="w-10 inline-block items-center" />
@@ -97,3 +97,4 @@ const Navbar = () => {
 
 
 export default Navbar; 
+
